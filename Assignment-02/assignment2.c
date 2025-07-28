d417cfafa424f7a3a8540ad3b1e1009651d9d6d9
Peteris <blankfeldspeteris@gmail.com>
2025-07-28 21:59:36 +0300

    02 finished

--- Assignment-02/assignment2.c
+++ Assignment-02/assignment2.c
@@ -3,11 +3,10 @@
 int main(void)
 {
   int integer = 5;
-  float floatvar = 3.14;
-  char string[] = "Hello, World!";
+  float floater = 4.321;
+  char character = '&';
 
-  printf("%d is an integer!\n", integer);
-  printf("%f is a float!\n", floatvar);
-  printf("%s is a char!\n", string);
-
+  printf("%d is an integer \n", integer);
+  printf("%f is a float \n", floater);
+  printf("%c is a char \n", character);
 }
